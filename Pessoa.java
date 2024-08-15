f5d7c3e50c2a3bb287717a3b0380f6f1af5d9b39
gtorresluiz <rm98600@fiap.com.br>
2024-08-14 22:41:06 -0300

    integrantes

--- Pessoa.java
+++ Pessoa.java
@@ -1,24 +1,30 @@
+// Eduardo Fedeli Souza RM:550132
+// Otavio Vitoriano da Silva RM:552012
+// Gabriel Torres Luiz RM:98600
+
 public class Pessoa {
     private String nome;
     private String documento;
     private int idade;
 
-    public Pessoa(String nome, String documento, int idade){
+    public Pessoa(String nome, String documento, int idade) {
         this.idade = idade;
         this.nome = nome;
         this.idade = idade;
     }
 
-    public String getNome(){
+    public String getNome() {
         return nome;
     }
-    public int getIdade(){
+
+    public int getIdade() {
         return idade;
     }
-    public String getDocumento(){
+
+    public String getDocumento() {
         return documento;
     }
-    
+
     public String toString() {
         return "Pessoa{" +
                 "nome='" + nome + '\'' +
